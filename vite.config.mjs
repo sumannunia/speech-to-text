2886183bdbd49e77daa3450e8dd8798d6c6417d9
sumannunia <51792560+sumannunia@users.noreply.github.com>
2025-09-26 16:48:52 +0530

    workflow and deployment changes

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,11 +10,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/speech-to-text/' : '/',
   plugins: [
     Vue({
       template: { transformAssetUrls },
     }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
     Vuetify(),
     Components(),
     Fonts({
